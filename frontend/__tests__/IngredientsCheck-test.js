9a2f780a6f49d9c91e60502ffb17f339c5de5767
dementey <dementeyyy@gmail.com>
2018-08-24 21:01:27 +0300

    test __snapshots__

---
+++ frontend/__tests__/IngredientsCheck-test.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import IngredientsCheck from '../src/Components/LandingPage/IngredientsCheck/IngredientsCheck';
+
+test('работа IngredientsCheck', () => {
+
+  const component = renderer.create(
+    <IngredientsCheck />
+  );
+
+  let componentTree=component.toJSON();
+  expect(componentTree).toMatchSnapshot();
+
+    
+});
