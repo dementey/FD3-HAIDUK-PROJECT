9da052083f44a519cc90fe79a82aeb415917b981
dementey <dementeyyy@gmail.com>
2018-08-25 08:26:57 +0300

    project ready

--- frontend/__tests__/ScrollToTopButton-test.js
+++ frontend/__tests__/ScrollToTopButton-test.js
@@ -1,27 +1,36 @@
-﻿import React from 'react';
-import PropTypes from 'prop-types';
-import ScrollButton from '../src/Components/LandingPage/SearchPage/ScrollToTopButton'
-import { shallow, mount, render } from 'enzyme';
-import renderer from 'react-test-renderer';
+﻿import 'core-js/es6/map';
+import 'core-js/es6/set';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Enzyme, { mount, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
-// и напишем наш тест для компонента
+
+import ScrollButton from '../src/Components/LandingPage/SearchPage/ScrollToTopButton'
+
+import 'raf/polyfill';
+
+
+
+Enzyme.configure({ adapter: new Adapter() })
 
 //*********************************
 
+// global.requestAnimationFrame = function (callback) {
+//   setTimeout(callback, 0)
+// }
 
-
-describe('>>>H O M E --- Shallow Render REACT COMPONENTS', () => {
+describe('>>>ScrollButton --- mount Render REACT COMPONENTS', () => {
   let wrapper;
-  const styleProps = {
-    scrollStepInPx: "80", delayInMs: "10"
-  };
+
   beforeEach(() => {
-    wrapper = shallow(<ScrollButton scrollStepInPx="80" delayInMs="10" />);
+    wrapper = mount(<ScrollButton key={100} scrollStepInPx="80" />);
 
   });
 
-  it('+++ contains output', () => {
-    expect(wrapper.props().styleProps).to.equal(styleProps);
+  it('+++ renders ScrollButton', () => {
+    expect(wrapper.props().scrollStepInPx).toEqual("80");
   });
-  console.log(wrapper.props());
+
 });
