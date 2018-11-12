6501d279c6d85094eee9bec000b999bc1af0c39b
dementey <dementeyyy@gmail.com>
2018-11-12 12:15:17 +0300

    connect to mLab MongoDB Sandbox

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/project"
-    //url: "mongodb://ds119820.mlab.com:19820/heroku_q68gdx4j"
+    //url: "mongodb://localhost:27017/project"
+    url: "mongodb://admin:heroku_1534126g@ds157493.mlab.com:57493/heroku_1534126g"
 }
